2781e31a58ab0034d1e190ac5c39963dc141db85
ecrseer <juustino@live.com>
2021-08-25 21:23:22 -0300

    baseurl

---
+++ src/const.js
@@ -0,0 +1,9 @@
+const ESTADOS = [
+  'RJ',
+  'SP',
+  'RS'
+]
+const baseUrlApi = "https://sheetdb.io/api/v1/cuyfdc2x1vwf4"
+
+export { ESTADOS, baseUrlApi }
+
---
+++ src/store.js
@@ -0,0 +1,76 @@
+import {createStore} from 'vuex'
+import { baseUrlApi } from './const'
+import axios from 'axios'
+
+const store = createStore({
+  state() {  // equivalente ao data de um componente
+    return {
+      carregando: false,
+      times: []
+    }
+  },
+  getters: { // equivalente ao computed de um componente
+
+  },
+  mutations: { // altera o state
+    carregando(state) {
+      state.carregando = true
+    },
+    time_carregado(state, times) {
+      state.times = times
+      state.carregando = false
+    },
+    time_apagar(state, time) {
+      let index = state.times.indexOf(time)
+      if (index >= 0) {
+        state.times.splice(index, 1)
+      }
+      state.carregando = false
+    },
+    time_editar(state, {original, editado}) {
+      Object.assign(original, editado)
+      state.carregando = false
+    },
+    time_criar(state, time) {
+      state.times.push(time)
+      state.carregando = false
+    }
+  },
+  actions: { // equivalente ao methods de um componente
+    async carregar({commit}) {
+      commit('carregando')
+
+      axios.get(`${baseUrlApi}`).then(({data}) => {
+        commit('time_carregado', data)
+      })
+    },
+    async apagar({commit}, time) {
+      commit('carregando')
+
+      await axios.delete(`${baseUrlApi}/id/${time.id}`)
+      commit('time_apagar', time)
+
+    },
+    async criar({commit}, time) {
+      commit('carregando')
+      await axios.post(
+        `${baseUrlApi}`,
+        {data: [time]}
+      )
+      commit('time_criar', time)
+
+    },
+    async editar({commit}, {original, editado}) {
+      commit('carregando')
+
+      await axios.put(
+        `${baseUrlApi}/id/${original.id}`,
+        {data: [editado]}
+      )
+      commit('time_editar', {original, editado})
+    }
+
+  }
+})
+
+export default store
